031440216fda25c2665072c04ad79573349dd5a6
Mario Ortas <mario.ortas@outlook.com>
2020-10-15 00:23:38 +0200

    TransactionProcessor implemented

--- 3.TransactionProcessor/src/TransactionProcessor.js
+++ 3.TransactionProcessor/src/TransactionProcessor.js
@@ -1,3 +1,5 @@
+const Transaction = require('./Transaction');
+
 class TransactionProcessor {
   // QUESTION: COMPLETE ALL CLASS FUNCTIONS TO PASS THE TESTS
 
@@ -13,38 +15,43 @@ class TransactionProcessor {
 
   // Check valid transactions rules
   static isValidTransaction(transaction) {
-    // ...
-    return true;
+    const t = new Transaction(transaction);
+    return t.isValid();
   }
 
   // Remove invalid transactions
   filterInvalidTransactions() {
-    // ...
+    const isValid = TransactionProcessor.isValidTransaction;
+    this.transactions = this.transactions.filter(t => !isValid(t));
     return this;
   }
 
   // Return transactions of given currency
   getTransactionsByCurrency(currency) {
-    // ...
+    this.transactions = this.transactions.filter(t => t.currency === currency);
     return this;
   }
 
   // Return transactions of given brand
   getTransactionsByBrand(brand) {
-    // ...
+    this.transactions = this.transactions.filter(t => t.brand === brand);
     return this;
   }
 
   // BONUS:
   // Apply multiple filters. Filters parameter should be an array of functions (predicates)
   filterTransaction(filters) {
-    // ...
+    if (!Array.isArray(filters)) return;
+
+    filters.map(f => {
+      if (f instanceof Function) this.transactions = this.transactions.filter(f);
+    });
     return this;
   }
 
   // Return the total amount of current transactions array
   sum() {
-    return 0;
+    return this.transactions.reduce((acc, t) => acc + t.amount, 0);
   }
 }
 
